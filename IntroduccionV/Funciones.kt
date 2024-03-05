1e8e116f38f818196325d1a4d66e8c03d9af2c36
ValentinaCorreaG <91170440+ValentinaCorreaG@users.noreply.github.com>
2024-03-05 18:59:45 -0500

    functions
    ejercicio de funciones y calculadora en clase 5/03

---
+++ IntroduccionV/Funciones.kt
@@ -0,0 +1,19 @@
+package IntroduccionV
+
+fun print(){
+    println("Hello Valentina")
+}
+fun hello(name:String?):Unit{
+    println("Hello " + name)
+}
+
+fun main(){
+    println("Hello, world!")
+
+
+fun suma(a:Int, b:Int) {
+    println(a + b)
+}
+hello("valentina")
+suma(1,5)
+}
---
+++ IntroduccionV/Intro.kt
@@ -0,0 +1,4 @@
+package IntroduccionV
+
+class Intro {
+}
