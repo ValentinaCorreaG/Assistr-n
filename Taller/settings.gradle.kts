a6746a19e87b70776b185dfde462c18bcf457659
Dannyfandi <daniel.fandino@urosario.edu.co>
2024-03-29 12:46:37 -0500

    Punto 5 taller
    Se dividio en 2 carpetas el repo, de proyecto y del taller y se subio el 5 reto del taller

---
+++ Taller/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Taller1"
+include(":app")
+ 
