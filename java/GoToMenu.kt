6bffead6bb12935cb6a957160e92d0b8ca9bfb9b
ValentinaCorreaG <91170440+ValentinaCorreaG@users.noreply.github.com>
2024-05-14 14:19:16 -0500

    Add files via upload

---
+++ java/GoToMenu.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.ComponentActivity
+import android.content.DialogInterface
+import android.widget.Toast
+
+class GoToMenu : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio)
+    }
+}
